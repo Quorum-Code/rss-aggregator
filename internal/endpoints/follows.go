be91940d04b62972b05f6c17efb77792228702ae
Quorum-Code <quorumcode@gmail.com>
2024-07-15 13:54:33 -0700

    Implemented endpoint for POST feed_follows

--- internal/endpoints/follows.go
+++ internal/endpoints/follows.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Quorum-Code/rss-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *ApiConfig) GetFeedFollows(resp http.ResponseWriter, req *http.Request, user database.User) {
@@ -17,7 +18,23 @@ func (cfg *ApiConfig) GetFeedFollows(resp http.ResponseWriter, req *http.Request
 }
 
 func (cfg *ApiConfig) PostFeedFollow(resp http.ResponseWriter, req *http.Request, user database.User) {
+	type body struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
 
+	var b body
+	jsonUnmarshalReader(req.Body, &b)
+
+	follow, err := cfg.DB.CreateFeedFollow(req.Context(), database.CreateFeedFollowParams{
+		FeedID: b.FeedID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(resp, http.StatusInternalServerError, err)
+		return
+	}
+
+	respondWithJSON(resp, http.StatusOK, follow)
 }
 
 func (cfg *ApiConfig) DeleteFeedFollow(resp http.ResponseWriter, req *http.Request, user database.User) {
