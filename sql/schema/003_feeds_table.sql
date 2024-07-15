003f6238e4c576bcc20a65142cf2cca95907323a
Quorum-Code <quorumcode@gmail.com>
2024-07-15 14:19:17 -0700

    Changed data types to all caps

--- sql/schema/003_feeds_table.sql
+++ sql/schema/003_feeds_table.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 CREATE TABLE feeds(
     id UUID PRIMARY KEY,
-    created_at timestamp NOT NULL,
-    updated_at timestamp NOT NULL,
-    name text NOT NULL,
-    url text NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT NOT NULL UNIQUE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
